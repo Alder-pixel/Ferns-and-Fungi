3832b60bb7beb0027b29e08c19bec3d0c3726f9c
alder-pixel <alderfulton@gmail.com>
2022-07-05 15:54:52 -0400

    Added makefile and changed analyze_images script to work with the Makefile and be a little more generalized.

---
+++ Makefile
@@ -0,0 +1,21 @@
+ROOT_DIR:=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))/
+INPUTS:=$(ROOT_DIR)/Data/Input/
+OUTPUTS:=$(ROOT_DIR)/Data/Output/
+
+# Analyze images
+analyze_images:
+	python $(ROOT_DIR)/Scripts/analyze_images.py $(ROOT_DIR)
+
+
+
+
+
+
+# Code to replicate:
+sync_local_to_remote_data:
+	rsync -ave ssh /home/scott/Documents/Uni/Research/Projects/Blueberry_Network_Rewiring/data --chmod=Dg+s teresisc@rsync.hpcc.msu.edu:/mnt/research/edgerpat_lab/Scotty/Blueberry_Network_Rewiring/
+
+sync_local_to_remote_results:
+	rsync -ave ssh /home/scott/Documents/Uni/Research/Projects/Blueberry_Network_Rewiring/results --chmod=Dg+s teresisc@rsync.hpcc.msu.edu:/mnt/research/edgerpat_lab/Scotty/Blueberry_Network_Rewiring/
+	
+
--- Scripts/analyze_images.py
+++ Scripts/analyze_images.py
@@ -3,72 +3,86 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import pandas as pd
+import argparse
+
 
 def find_area(directory, picture, project_directory):
     black_area = 9  # cm^2. This is a magic number from me measuring the square
-                    # Included in the image.
+    # Included in the image.
     img = mpimg.imread(directory + picture)
     image_copy = img.copy()
     black_mask = np.sum(img, axis=2) < 120
     image_copy[black_mask] = 255
-    
-    green_mask = (1.25 * image_copy[:,:,0] > image_copy[:,:,1]) &\
-            (1.25 * image_copy[:,:,2] > image_copy[:,:,1])
+
+    green_mask = (1.25 * image_copy[:, :, 0] > image_copy[:, :, 1]) & (
+        1.25 * image_copy[:, :, 2] > image_copy[:, :, 1]
+    )
     green_mask = np.invert(green_mask)
-    
+
     #  Save the images to check if they make sense.
-    plt.figure(figsize = (30,15))
-    plt.subplot(1,2,1)
+    plt.figure(figsize=(30, 15))
+    plt.subplot(1, 2, 1)
     plt.imshow(img)
-    plt.subplot(1,2,2)
+    plt.subplot(1, 2, 2)
     plt.imshow(image_copy)
-    plt.savefig(project_directory + "Data/Output/See_black/" + picture[:-4] + "_black.png")
+    plt.savefig(
+        project_directory + "Data/Output/See_black/" + picture[:-4] + "_black.png"
+    )
     plt.close()
 
     image_copy[green_mask] = 255
     image_copy[np.invert(green_mask)] = 0
-    plt.figure(figsize = (30,15))
-    plt.subplot(1,2,1)
+    plt.figure(figsize=(30, 15))
+    plt.subplot(1, 2, 1)
     plt.imshow(img)
-    plt.subplot(1,2,2)
+    plt.subplot(1, 2, 2)
     plt.imshow(image_copy)
-    plt.savefig(project_directory + "Data/Output/See_green/" + picture[:-4] + "_green.png")
+    plt.savefig(
+        project_directory + "Data/Output/See_green/" + picture[:-4] + "_green.png"
+    )
     plt.close()
 
     return 9 / black_mask.sum() * green_mask.sum()
 
 
-project_directory = "/home/alder/Documents/research/ferns_and_fungi/Ferns-and-Fungi/"
-picture_directory = project_directory + "Data/Input/Pictures/"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("root_dir", type=str, help="Path to the projects directory")
+    args = parser.parse_args()
 
-growth_dict = {
-    "ID": [],
-    "Fungus": [],
-    "Endobacteria": [],
-    "Inoculation time" : [],
-    "Replicate Number" : [],
-    "Day of Picture" : [],
-    "Area" : []
-}
-for picture in os.listdir(picture_directory):
-    split_name = picture.split("_")
-    split_name[-1] = split_name[-1][:-4]
+    project_directory = args.root_dir
+    picture_directory = project_directory + "Data/Input/Pictures/"
 
-    growth_dict["ID"].append(picture[:-4])
-    growth_dict["Fungus"].append(split_name[0])
+    growth_dict = {
+        "ID": [],
+        "Fungus": [],
+        "Endobacteria": [],
+        "Inoculation time": [],
+        "Replicate Number": [],
+        "Day of Picture": [],
+        "Area": [],
+    }
+    for picture in os.listdir(picture_directory):
+        split_name = picture.split("_")
+        split_name[-1] = split_name[-1][:-4]
 
-    if split_name[0] == "control":
-       growth_dict["Endobacteria"].append(np.nan)
-       growth_dict["Inoculation time"].append(np.nan)
-       growth_dict["Replicate Number"].append(split_name[1])
-       growth_dict["Day of Picture"].append(int(split_name[2][-1]))
-    else:
-       growth_dict["Endobacteria"].append(split_name[0][-1])
-       growth_dict["Inoculation time"].append(split_name[1])
-       growth_dict["Replicate Number"].append(split_name[2])
-       growth_dict["Day of Picture"].append(int(split_name[3][-1]))
-    growth_dict["Area"].append(find_area(picture_directory, picture, project_directory))
+        growth_dict["ID"].append(picture[:-4])
+        growth_dict["Fungus"].append(split_name[0])
 
-growth_df = pd.DataFrame(growth_dict)
-print(growth_df)
-growth_df.to_csv(project_directory + "Data/Output/fern_growth.csv")
+        if split_name[0] == "control":
+            growth_dict["Endobacteria"].append(np.nan)
+            growth_dict["Inoculation time"].append(np.nan)
+            growth_dict["Replicate Number"].append(split_name[1])
+            growth_dict["Day of Picture"].append(int(split_name[2][-1]))
+        else:
+            growth_dict["Endobacteria"].append(split_name[0][-1])
+            growth_dict["Inoculation time"].append(split_name[1])
+            growth_dict["Replicate Number"].append(split_name[2])
+            growth_dict["Day of Picture"].append(int(split_name[3][-1]))
+        growth_dict["Area"].append(
+            find_area(picture_directory, picture, project_directory)
+        )
+
+    growth_df = pd.DataFrame(growth_dict)
+    print(growth_df)
+    growth_df.to_csv(project_directory + "Data/Output/fern_growth.csv")
